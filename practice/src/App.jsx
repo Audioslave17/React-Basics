5f1cdd6422a220af8798ff57ec1fc7f2edb00c7c
Anurag Kumar <114430275+Audioslave17@users.noreply.github.com>
2024-07-02 19:35:37 +0530

    balh

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,26 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [val, setVal] = useState(10);
-  const addValue = () => {
-    if(val<20){
-      val = val + 1;
-      setVal(val);
-    }
-  }
-  const removeValue = () => {
-    if(val>0){
-      val = val - 1;
-      setVal(val);
-    }
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [total,setTotal] = useState();
+  const add = () => {
+    setTotal(num1 + num2);
   }
   return (
     <div>
-      <h1>Counter Application</h1>
-      <p>Current value: {val}</p>
-
-      <button onClick={addValue}>add</button>
-      <button onClick={removeValue}>remove</button>
+      <input type="number" value={num1} onChange={(e)=>setNum1(+e.target.value)}/>
+      <input type="number" value={num2} onChange={(e)=>setNum2(+e.target.value)}/>
+      <button onClick={add}>add</button>
+      <p>{total}</p>
     </div>
   )
 }
